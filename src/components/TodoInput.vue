1f4392491aadd79df447499d97c32fa72e2aaccb
Parkjunwoo <sinyoorang@gmail.com>
2019-06-02 02:25:54 +0900

    컴포넌트 분리

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <input type="text" v-model="newTodoItem">
+        <p @click="addTodo"><i class="fas fa-plus"></i></p>
+        <button @click="addTodo">Add</button>
+    </div>    
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if(this.newTodoItem !== "") {
+                var value = this.newTodoItem.trim();
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = '';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
