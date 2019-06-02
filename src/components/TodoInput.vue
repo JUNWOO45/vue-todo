93a01a9bc3d53380a459f21c06b439b5e9da001d
Parkjunwoo <sinyoorang@gmail.com>
2019-06-02 22:05:18 +0900

    input버튼 생성

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,8 +1,9 @@
 <template>
-    <div>
-        <input type="text" v-model="newTodoItem">
-        <p @click="addTodo"><i class="fas fa-plus"></i></p>
-        <button @click="addTodo">Add</button>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" placeholder="Type what to do" v-on:keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="addBtn fa fa-plus" aria-hidden="true"></i>
+        </span>
     </div>    
 </template>
 
@@ -15,6 +16,7 @@ export default {
     },
     methods: {
         addTodo() {
+            console.log("clicked!")
             if(this.newTodoItem !== "") {
                 var value = this.newTodoItem.trim();
                 localStorage.setItem(value, value);
@@ -29,5 +31,28 @@ export default {
 </script>
 
 <style>
-
+    input:focus {
+        outline: none;
+    }
+    .inputBox {
+        background: white;
+        height: 50px;
+        line-height: 50px;
+        border-radius: 5px;
+    }
+    .inputBox input {
+        border-style: none;
+        font-size: 0.9rem;
+    }
+    .addContainer {
+        float:right;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+        display: inline-block;
+        width: 3rem;
+        border-radius: 0 5px 5px 0;
+    }
+    .addBtn {
+        color: white;
+        vertical-align: middle;
+    }
 </style>
